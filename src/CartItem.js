e27d8f7b63f639bfb6b4218ee6e4db4ca3ebe1cb
rahuljha661 <rahuljha12527@gmail.com>
2020-10-19 23:14:23 +0530

    add index.css

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,31 +1,37 @@
 import React from "react";
 
 class CartItem extends React.Component {
-    constructor(){
-        super();
-        this.state={  
-            price:999,
-            title:'Phone',
-            qty:1,
-            img:''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      price: 999,
+      title: "Phone",
+      qty: 1,
+      img: "",
+    };
+    this.increaseQuantity=this.increaseQuantity.bind(this);
+  }
+  increaseQuantity=()=> {
+    console.log("this.state",this.state);
+    
+  }
   render() {
-    const {price,title,qty}=this.state;
+    const { price, title, qty } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
           <img style={styles.image} />
         </div>
         <div className="right-block">
-    <div style={{ fontSize: 25 }}>{title}</div>
-    <div style={{ color: "#777" }}>Rs  {price}</div>
-    <div style={{ color: "#777" }}>Qty: {qty}</div>
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: "#777" }}>Rs {price}</div>
+          <div style={{ color: "#777" }}>Qty: {qty}</div>
           <div className="cart-item-actions">
             <img
               alt="increase"
               className="action-icons"
               src="https://as2.ftcdn.net/jpg/01/07/62/07/500_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg"
+              onClick={this.increaseQuantity.bind(this)}
             />
             <img
               alt="decrease"
